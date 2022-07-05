745b36170e85dc1df07b4c1c4710f8ba910527da
Romina2219 <104035349+Romina2219@users.noreply.github.com>
2022-07-05 08:01:00 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+
+function inclusive(){
+  document.body.style.backgroundColor = "black";
+  document.getElementById("1").style.backgroundColor = "black";
+  document.getElementById("1").style.color = "white";
+  document.getElementById("2").style.backgroundColor = "black";
+  document.getElementById("2").style.color = "white";
+  document.getElementById("3").style.backgroundColor = "black";
+  document.getElementById("3").style.color = "white";
+  document.getElementById("4").style.backgroundColor = "black";
+  document.getElementById("4").style.color = "white";
+  document.getElementById("bienvenidos").style.color = "white";
+  document.getElementById("p1").style.color = "white";
+  document.getElementById("p2").style.color = "white";
+  document.getElementById("p3").style.color = "white";
+  document.getElementById("p4").style.color = "white";
+  document.getElementById("p5").style.color = "white";
+  document.getElementById("p6").style.color = "white";
+  document.getElementById("p7").style.color = "white";
+}
+function cartel() {alert("Romina Romero 2022")}
